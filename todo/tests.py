30134e5d74d8b0fd0b093a911f88833f54852ec9
1F10250023 <s1f102500236@iniad.org>
2026-06-25 11:14:59 +0900

    足りないところを追加

--- todo/tests.py
+++ todo/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
+from django.test import Client
 
 
 # Create your tests here.
@@ -34,6 +35,17 @@ class TaskModelTestCase(TestCase):
         task.save()
 
         self.assertFalse(task.is_overdue(current))
+    def test_is_overdue_past(self):
+        due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
+        current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
+        task = Task(title='task2', due_at=due)
+        task.save()
+        self.assertTrue(task.is_overdue(current))
+    def test_is_overdue_none(self):
+        current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
+        task = Task(title='task3')
+        task.save()
+        self.assertFalse(task.is_overdue(current))
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
         client = Client()
@@ -45,7 +57,7 @@ class TodoViewTestCase(TestCase):
     def test_index_post(self):
         client = Client()
         data = {'title':'Test Task', 'due_at': '2024-06-30 23:59:59'}
-        response = Client.post('/', data)
+        response = client.post('/', data)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
