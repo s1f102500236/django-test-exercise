fe15a4b4b7f16019c40a174de5cc2b6c8ce5ca41
1F10250192 <s1f102501923@iniad.org>
2026-07-09 11:31:08 +0900

    削除機能のテストを追加

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,6 @@ from todo import views as todo_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', todo_views.index, name='index'),
+    path('<int:task_id>/delete/', todo_views.delete, name='delete'),
     path('<int:task_id>/', todo_views.detail, name='detail'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import Http404, HttpResponseNotAllowed
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
@@ -33,3 +33,12 @@ def detail(request, task_id):
         'task': task,
     }
     return render(request, 'todo/detail.html', context)
+
+
+def delete(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    if request.method != 'POST':
+        return HttpResponseNotAllowed(['POST'])
+
+    task.delete()
+    return redirect('index')
