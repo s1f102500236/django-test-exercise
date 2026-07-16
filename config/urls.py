d9983065833a9439cea4001dd5ca8e090cece35a
1f10250006 <s1f102500067@iniad.org>
2026-07-16 12:05:55 +0900

    Aboutページを追加

--- config/urls.py
+++ config/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('<int:task_id>/delete/', todo_views.delete, name='delete'),
     path('<int:task_id>/', todo_views.detail, name='detail'),
     path('<int:task_id>/update', todo_views.update, name='update'),
-    path('<int:task_id>/delete', todo_views.delete, name='delete')
+    path('<int:task_id>/delete', todo_views.delete, name='delete'),
+    path('about/', todo_views.about, name='about'),
 ]
